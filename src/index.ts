7de05059175fc81caf3244e386d036113053e798
Developer <dev@example.com>
2026-03-02 20:58:44 -0300

    chore: add zod and fastify-zod-provider setup

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,39 @@
 import "dotenv/config";
 
 import Fastify from "fastify";
-const fastify = Fastify({
-  logger: true,
-});
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
 
-fastify.get("/", async function handler() {
-  return { hello: "world" };
+const app = Fastify({ logger: true });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/",
+
+  schema: {
+    description: "Hello World",
+    tags: ["hello world"],
+    response: {
+      200: z.object({
+        message: z.string(),
+      }),
+    },
+  },
+  handler: () => {
+    return { message: "Hello World" };
+  },
 });
 
 try {
-  await fastify.listen({ port: Number(process.env.PORT) || 8082 });
+  await app.listen({ port: Number(process.env.PORT) || 8082 });
 } catch (err) {
-  fastify.log.error(err);
+  app.log.error(err);
   process.exit(1);
 }
