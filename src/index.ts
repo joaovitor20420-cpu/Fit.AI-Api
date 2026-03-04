fb2b935add5abb3009777f30f8030a1615022b93
Developer <dev@example.com>
2026-03-04 03:17:52 -0300

    feat: add creat wourkout plan endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import fastifyCors from "@fastify/cors";
 import fastifySwagger from "@fastify/swagger";
 import fastifyApiReference from "@scalar/fastify-api-reference";
+import { fromNodeHeaders } from "better-auth/node";
 import Fastify from "fastify";
 import {
   jsonSchemaTransform,
@@ -10,8 +11,11 @@ import {
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import z from "zod";
 
+import { WeekDay } from "./generated/prisma/enums.js";
 import { auth } from "./lib/auth.js";
+import { CreateWorkoutPlan } from "./usecases/CreateWorkoutPlan.js";
 
 const app = Fastify({ logger: true });
 
@@ -53,6 +57,78 @@ await app.register(fastifyApiReference, {
         url: "/api/auth/open-api/generate-schema",
       },
     ],
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "POST",
+  url: "/workout-plans",
+  schema: {
+    body: z.object({
+      name: z.string().trim().min(1),
+      workoutDays: z.array(
+        z.object({
+          name: z.string().trim().min(1),
+          weekDay: z.nativeEnum(WeekDay),
+          isRest: z.boolean().default(false),
+          estimatedDurationInSeconds: z.number().min(1),
+          exercises: z.array(
+            z.object({
+              order: z.number().min(0),
+              name: z.string().trim().min(1),
+              sets: z.number().min(1),
+              reps: z.number().min(1),
+              restTimeInSeconds: z.number().min(1),
+            }),
+          ),
+        }),
+      ),
+    }),
+    response: {
+      201: z.object({
+        id: z.uuid(),
+        name: z.string().trim().min(1),
+        workoutDays: z.array(
+          z.object({
+            name: z.string().trim().min(1),
+            weekDay: z.nativeEnum(WeekDay),
+            isRest: z.boolean().default(false),
+            estimatedDurationInSeconds: z.number().min(1),
+            exercises: z.array(
+              z.object({
+                order: z.number().min(0),
+                name: z.string().trim().min(1),
+                sets: z.number().min(1),
+                reps: z.number().min(1),
+                restTimeInSeconds: z.number().min(1),
+              }),
+            ),
+          }),
+        ),
+      }),
+      401: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+    },
+  },
+  handler: async (request, reply) => {
+    const session = await auth.api.getSession({
+      headers: fromNodeHeaders(request.headers),
+    });
+    if (!session) {
+      return reply.status(401).send({
+        error: "Unauthorized",
+        code: "UNAUTHORIZED",
+      });
+    }
+    const createWorkoutPlan = new CreateWorkoutPlan();
+    const result = await createWorkoutPlan.execute({
+      userId: session.user.id,
+      name: request.body.name,
+      workoutDays: request.body.workoutDays,
+    });
+    return reply.status(201).send(result);
   },
 });
 
---
+++ src/usecases/CreateWorkoutPlan.ts
@@ -0,0 +1,114 @@
+import { WeekDay } from "../generated/prisma/enums.js";
+import { prisma } from "../lib/db.js";
+
+interface InputDto {
+  userId: string;
+  name: string;
+  workoutDays: Array<{
+    name: string;
+    weekDay: WeekDay;
+    isRest: boolean;
+    estimatedDurationInSeconds: number;
+    exercises: Array<{
+      order: number;
+      name: string;
+      sets: number;
+      reps: number;
+      restTimeInSeconds: number;
+    }>;
+  }>;
+}
+
+export interface OutputDto {
+  id: string;
+  name: string;
+  workoutDays: Array<{
+    name: string;
+    weekDay: WeekDay;
+    isRest: boolean;
+    estimatedDurationInSeconds: number;
+    exercises: Array<{
+      order: number;
+      name: string;
+      sets: number;
+      reps: number;
+      restTimeInSeconds: number;
+    }>;
+  }>;
+}
+export class CreateWorkoutPlan {
+  async execute(dto: InputDto): Promise<OutputDto> {
+    const existingWorkoutPlan = await prisma.workoutPlan.findFirst({
+      where: {
+        isActive: true,
+      },
+    });
+    return prisma.$transaction(async (tx) => {
+      if (existingWorkoutPlan) {
+        await tx.workoutPlan.update({
+          where: {
+            id: existingWorkoutPlan.id,
+          },
+          data: {
+            isActive: false,
+          },
+        });
+      }
+      const createdPlan = await tx.workoutPlan.create({
+        data: {
+          name: dto.name,
+          userId: dto.userId,
+          isActive: true,
+          workoutDays: {
+            create: dto.workoutDays.map((workoutDay) => ({
+              name: workoutDay.name,
+              weekDay: workoutDay.weekDay,
+              isRest: workoutDay.isRest,
+              estimatedDurationSeconds: workoutDay.estimatedDurationInSeconds,
+              workoutExercises: {
+                create: workoutDay.exercises.map((exercise) => ({
+                  order: exercise.order,
+                  name: exercise.name,
+                  sets: exercise.sets,
+                  reps: exercise.reps,
+                  restTimeInSeconds: exercise.restTimeInSeconds,
+                })),
+              },
+            })),
+          },
+        },
+      });
+      const plan = await tx.workoutPlan.findUnique({
+        where: { id: createdPlan.id },
+        include: {
+          workoutDays: {
+            include: {
+              workoutExercises: true,
+            },
+          },
+        },
+      });
+
+      if (!plan) {
+        throw new Error("Workout plan not found");
+      }
+      return {
+        id: plan.id,
+        name: plan.name,
+        workoutDays: plan.workoutDays.map((day) => ({
+          name: day.name,
+          weekDay: day.weekDay,
+          isRest: day.isRest,
+          estimatedDurationInSeconds: day.estimatedDurationSeconds,
+          exercises: day.workoutExercises.map((ex) => ({
+            order: ex.order,
+            name: ex.name,
+            sets: ex.sets,
+            reps: ex.reps,
+            restTimeInSeconds: ex.restTimeInSeconds,
+          })),
+        })),
+      };
+    });
+  }
+}
